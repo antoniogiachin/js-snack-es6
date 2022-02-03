ef0ad4dff4e74db07187e4c8181114cab0a2392b
antoniogiachin <giachinantonio@outlook.com>
2022-02-03 18:56:06 +0100

    fine snack1

--- Snack1/main.js
+++ Snack1/main.js
@@ -23,8 +23,25 @@ const bici = [
 ];
 
 
+const arrayPesi =[];
+
+for (let i = 0; i < bici.length; i++){
+
+    let {peso} = bici[i];
+    
+    let nomePeso = `La bici ${bici[i].nome} pesa ${(peso)} kg`;
+
+    console.log(nomePeso);
+
+    arrayPesi.push(parseInt(peso));
 
 
+}
+
+
+console.log(arrayPesi);
+const result = Math.min(...arrayPesi);
+console.log(`Il peso minore è di ${result} kg`);
 
 
 
