af1d1c4d383f12c87868f152af75b143d4d0f1c1
antoniogiachin <giachinantonio@outlook.com>
2022-02-04 09:22:24 +0100

    fine esercizio

--- Snack1/main.js
+++ Snack1/main.js
@@ -1,49 +1,70 @@
-// Creo array oggetti bici
-
 const bici = [
 
     {
-        nome:'era',
+        nome : 'Rotter',
 
-        peso: 25,
+        peso : 20,
     },
 
     {
-        nome:'ea',
+        nome : 'Terren',
 
         peso: 15,
     },
 
-    {
-        nome:'esa',
 
-        peso: 20,
-    }
+    {
+        nome: 'Setter',
+
+        peso: 25,
+    },
 
 ];
 
 
-const arrayPesi =[];
 
-for (let i = 0; i < bici.length; i++){
+// Prendo i valori del primo oggetto dell'array
 
-    let {peso} = bici[i];
+function lightestProduct(myArrayOfProducts){
+
+    let {peso} = myArrayOfProducts[0];
+
+    let {nome} = myArrayOfProducts[0];
+
+    for(let i = 0; i < myArrayOfProducts.length; i++){
+
+        if(peso > myArrayOfProducts[i].peso){
+            peso = myArrayOfProducts[i].peso;
+            nome = myArrayOfProducts[i].nome;
+        }
+
+    }
+
+    let risultato = `Il prodotto che pesa meno è ${nome} che pesa ${peso} kg`;
+
+    return risultato;
     
-    let nomePeso = `La bici ${bici[i].nome} pesa ${(peso)} kg`;
-
-    console.log(nomePeso);
-
-    arrayPesi.push(parseInt(peso));
-
-
 }
 
 
-console.log(arrayPesi);
-const result = Math.min(...arrayPesi);
-console.log(`Il peso minore è di ${result} kg`);
+let final = lightestProduct(bici);
+console.log(final);
+
+// let {peso} = bici[0];
+// let {nome} = bici [0];
 
 
+// // Ciclo valori array
+// for(let i = 0; i < bici.length; i++){
 
+//     if(peso > bici[i].peso){
+//         peso = bici[i].peso;
+//         nome = bici[i].nome;
+//     }
 
+//     /*sto dicendo: se la variabile peso con 0 è maggiore della successiva
+//     allora peso e nome sono quelli della successiva, poi ricomincia, quella finale sarà ovviamente la più piccola*/
 
+// }
+
+// console.log(`La bicicletta con il peso minore è ${nome} che pesa ${peso} kg `);
